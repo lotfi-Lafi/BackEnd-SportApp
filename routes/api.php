db7fcf9d8c6a062a5a882b4d851077c33e1c00b5
lotfi-Lafi <lotfi.lafi2@gmail.com>
2017-03-27 11:23:19 +0200

    headers

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 });
 
-header('Access-Control-Allow-Origin: http://localhost:8081/');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+//header('Access-Control-Allow-Origin: http://127.0.0.1:8081');
+//header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
-// authenticate : 
+// authenticate 0
 Route::post('authenticate', 'AuthenticateController@authenticate');
 
 /**************************************** Admin ****************************************/
