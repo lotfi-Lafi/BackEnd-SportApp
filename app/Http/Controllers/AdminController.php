9b30a039f7b9cc70bb715d090c09f36f7588ca80
lotfi-Lafi <lotfi.lafi2@gmail.com>
2017-04-13 21:14:16 +0200

    push notification laravel 3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -88,7 +88,7 @@ class AdminController extends Controller
 
         $push->setMessage(['message'=>'Hello World'])
                 ->setApiKey('AAAAqyAkYnE:APA91bGeKs2GT74IG_jCauw7EevaRZJ77CojxCRd3QpbyZ6smEmfjU451iS0ZuhdBUCKpy21KYAi8EENiCJL_AP-vaXL8jJdoH9uNb3g-jVtYWJO4G1kEyLaae4dRAuY3o7OXERLkL_c')
-                ->setDevicesToken([$user->tokenDevice]);
+                ->setDevicesToken([$user->tokenDevice])
                 ->setUrl('https://fcm.googleapis.com/fcm/send')
                 ->setConfig(['dry_run' => true]);
 
